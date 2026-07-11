c1172b45f95f25bd32cb5bcedf1457511aaa6766
Aliaksei <ac2oder@gmail.com>
2026-07-11 20:39:15 +0300

    Initial Subly React App

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Subly</h1>
+      <p>Learn languages through subtitles</p>
+
+      <button>
+        Upload subtitles
+      </button>
+    </div>
+  )
+}
+
+export default App
