11f6a7886c866cb2e067822f5e6a99132ff64a58
Aliaksei <ac2oder@gmail.com>
2026-07-13 20:27:29 +0300

    add translation services

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,32 @@
 import { useState } from "react";
+import { translateWord } from "./services/translationApi"
 import "./App.css";
 
 function App() {
   const [text, setText] = useState("");
+  const [translation, setTranslation] = useState("");
+
+  function handleChange(event) {
+    setText(event.target.value);
+  }
+
+  async function handleTranslate() {
+    const result = await translateWord(text);
+    setTranslation(result);
+  }
 
   return (
     <div className="app">
       <input
         type="text"
         value={text}
-        onChange={(event) => setText(event.target.value)}
+        onChange={handleChange}
       />
 
-      <p>{text}</p>
+      <button onClick={handleTranslate}>
+        Translate
+      </button>
+      <p>{translation}</p>
     </div>
   );
 }
